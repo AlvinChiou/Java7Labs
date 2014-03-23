62af16ab5ab226c09c586634f3383efe85b70f21
AlvinChiou <alvinhsian@gmail.com>
2014-03-23 21:47:18 +0800

    This is program can encrypt and verify your password.

--- src/MessageDigest/StringEncryptMain.java
+++ src/MessageDigest/StringEncryptMain.java
@@ -2,9 +2,9 @@ package MessageDigest;
 public class StringEncryptMain {
     public static void main(String[] args) {
         StringEncrypt se = new StringEncrypt();
-        String PlanPassword = "AlvinChiou";//由此傳入須加密的資料，型態為字串。
+        String PlainPassword = "AlvinChiou";//由此傳入須加密的資料，型態為字串。
         String DataBasePassword = ""; //由此傳入從資料庫取出被加密的密碼，型態為字串。
-        se.StringEncrypt(PlanPassword);
+        se.StringEncrypt(PlainPassword);
         System.out.println("被加密後的資料為:" + se.getEncryptData());
         //直接將se.getEncryptData()傳回的資料寫入資料庫就實現資料庫密碼加密囉!!
 
