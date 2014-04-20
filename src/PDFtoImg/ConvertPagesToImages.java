89d40a3cd1813ab0db66c64242d5f5dc7aa7e176
AlvinChiou <alvinhsian@gmail.com>
2014-04-21 03:07:10 +0800

    http://stackoverflow.com/questions/21064270/how-to-increase-the-resolution-of-image-in-pdf-renderer

--- src/PDFtoImg/ConvertPagesToImages.java
+++ src/PDFtoImg/ConvertPagesToImages.java
@@ -3,112 +3,70 @@ package PDFtoImg;
 /**
  * Created by Alvin on 2014/4/21.
  */
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import javax.imageio.ImageIO;
+
 import com.sun.pdfview.PDFFile;
 import com.sun.pdfview.PDFPage;
 
-import java.awt.Graphics;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.HeadlessException;
-import java.awt.Image;
-import java.awt.Rectangle;
-import java.awt.Transparency;
-import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import javax.swing.*;
-import javax.imageio.*;
-import java.awt.image.*;
-
 public class ConvertPagesToImages {
-
-    public static void setup() throws IOException {
-
-        //load a pdf from a byte buffer
-        File file = new File("H:\\Google-Mapreduce.pdf");
-        RandomAccessFile raf = new RandomAccessFile(file, "r");
-        FileChannel channel = raf.getChannel();
-        ByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
-        PDFFile pdffile = new PDFFile(buf);
-
-        int numPgs = pdffile.getNumPages();
-
-        for (int i=0; i<numPgs; i++)
-        {
-            // draw the first page to an image
-            PDFPage page = pdffile.getPage(i);
-
-            //get the width and height for the doc at the default zoom
-            Rectangle rect = new Rectangle(0,0,
-                    (int)page.getBBox().getWidth(),
-                    (int)page.getBBox().getHeight());
-
-            //generate the image
-            Image img = page.getImage(
-                    rect.width, rect.height, //width & height
-                    rect, // clip rect
-                    null, // null for the ImageObserver
-                    true, // fill background with white
-                    true  // block until drawing is done
-            );
-
-            //save it as a file
-            BufferedImage bImg = toBufferedImage( img );
-            File yourImageFile = new File("H:\\page_" + i + ".png");
-            ImageIO.write( bImg,"png",yourImageFile);
-        }
-    }
-
-    // This method returns a buffered image with the contents of an image
-    public static BufferedImage toBufferedImage(Image image) {
-        if (image instanceof BufferedImage) {
-            return (BufferedImage)image;
-        }
-
-        // This code ensures that all the pixels in the image are loaded
-        image = new ImageIcon(image).getImage();
-
-        // Create a buffered image with a format that's compatible with the screen
-        BufferedImage bimage = null;
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    public static void main(String[] args) {
         try {
-            // Determine the type of transparency of the new buffered image
-            int transparency = Transparency.OPAQUE;
+            String sourceDir = "H:/PDFCopy/Google-Mapreduce.pdf";// PDF file must be placed in DataGet folder
+            String destinationDir = "H:/PDFCopy/Converted/";//Converted PDF page saved in this folder
 
-            // Create the buffered image
-            GraphicsDevice gs = ge.getDefaultScreenDevice();
-            GraphicsConfiguration gc = gs.getDefaultConfiguration();
-            bimage = gc.createCompatibleImage(
-                    image.getWidth(null), image.getHeight(null), transparency);
-        } catch (HeadlessException e) {
-            System.out.println("The system does not have a screen");
-        }
+            File sourceFile = new File(sourceDir);
+            File destinationFile = new File(destinationDir);
 
-        if (bimage == null) {
-            int type = BufferedImage.TYPE_INT_RGB;
-            bimage = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
-        }
-
-        // Copy image to buffered image
-        Graphics g = bimage.createGraphics();
-
-        // Paint the image onto the buffered image
-        g.drawImage(image, 0, 0, null);
-        g.dispose();
-
-        return bimage;
-    }
-
-    public static void main(final String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    ConvertPagesToImages.setup();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
+            String fileName = sourceFile.getName().replace(".pdf", "");
+            if (sourceFile.exists()) {
+                if (!destinationFile.exists()) {
+                    destinationFile.mkdir();
+                    System.out.println("Folder created in: "+ destinationFile.getCanonicalPath());
                 }
+
+                RandomAccessFile raf = new RandomAccessFile(sourceFile, "r");
+                FileChannel channel = raf.getChannel();
+                ByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+                PDFFile pdf = new PDFFile(buf);
+                System.out.println("Total Pages: "+ pdf.getNumPages());
+                int pageNumber = 1;
+                for (int i = 0; i < pdf.getNumPages(); i++) {
+                    PDFPage page = pdf.getPage(i);
+
+                    // image dimensions
+                    int width = 1200;
+                    int height = 1400;
+
+                    // create the image
+                    Rectangle rect = new Rectangle(0, 0, (int) page.getBBox().getWidth(), (int) page.getBBox().getHeight());
+                    BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+                    // width & height, // clip rect, // null for the ImageObserver, // fill background with white, // block until drawing is done
+                    Image image = page.getImage(width, height, rect, null, true, true );
+                    Graphics2D bufImageGraphics = bufferedImage.createGraphics();
+                    bufImageGraphics.drawImage(image, 0, 0, null);
+
+                    File imageFile = new File( destinationDir + fileName +"_"+ pageNumber +".png" );// change file format here. Ex: .png, .jpg, .jpeg, .gif, .bmp
+
+                    ImageIO.write(bufferedImage, "png", imageFile);
+                    pageNumber++;
+
+                    System.out.println(imageFile.getName() +" File created in Folder: "+ destinationFile.getCanonicalPath());
+                }
+            } else {
+                System.err.println(sourceFile.getName() +" File not exists");
             }
-        });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
